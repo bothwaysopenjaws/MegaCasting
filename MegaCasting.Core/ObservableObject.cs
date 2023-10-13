d9360de4f77e98330ab1f70531f287958cca2616
morph <emmanuel.stephant@laposte.net>
2023-10-13 14:24:49 +0200

    Ajoutez des fichiers projet.

---
+++ MegaCasting.Core/ObservableObject.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+
+namespace MegaCasting.Core
+{
+    /// <summary>
+    /// Classe rendant un objet observable
+    /// </summary>
+    public abstract class ObservableObject : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        #region Events
+
+        /// <summary>
+        /// Se produit quand on modifie une propriété
+        /// </summary>
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        /// <summary>
+        /// Se produit quand la propriété à été changée
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Déclenche l'événement <see cref="PropertyChanging"/>
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanging(string propertyName)
+        {
+            this.PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+        /// <summary>
+        /// Déclenche l'événement <see cref="PropertyChanged"/>
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        /// <summary>
+        /// Assigne une propriété et déclenche les événements <see cref="PropertyChanging"/> puis <see cref="PropertyChanged"/> 
+        /// </summary>
+        /// <typeparam name="T">Type de la propriété</typeparam>
+        /// <param name="propertyName">Nom de la propriété</param>
+        /// <param name="field">Référence de l'attribut à assigner</param>
+        /// <param name="value"> Valeur</param>
+        protected void SetProperty<T>(string propertyName, ref T field, T value)
+        {
+            if ((field == null && value != null)
+                || (field?.Equals(value) == false))
+            {
+                OnPropertyChanging(propertyName);
+                field = value;
+                OnPropertyChanged(propertyName);
+
+            }
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ MegaCasting.DBLib/Models/MegaCastingContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCasting.DBLib.Models
+{
+    /// <summary>
+    /// Contexte megaCasting
+    /// </summary>
+    public class MegaCastingContext : DbContext
+    {
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connectionString");
+        }
+
+        #endregion
+
+    }
+}
